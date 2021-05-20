1ecc0a2098f58f42f6d3f77713f05d93ddd491eb
1stTimeGitHub <afancyusername@outlook.com>
2021-05-20 23:47:28 +0100

    GUI now support highlights

--- src/main/kotlin/GUI.kt
+++ src/main/kotlin/GUI.kt
@@ -1,6 +1,7 @@
 import org.eclipse.swt.SWT
 import org.eclipse.swt.events.SelectionAdapter
 import org.eclipse.swt.events.SelectionEvent
+import org.eclipse.swt.graphics.Color
 import org.eclipse.swt.layout.GridData
 import org.eclipse.swt.layout.GridLayout
 import org.eclipse.swt.widgets.*
@@ -9,6 +10,7 @@ class GUI private constructor(component: JSONComponent) {
 
     private val shell: Shell
     private val tree: Tree
+    private val textField: Text
     private val label: Label
 
     companion object {
@@ -21,21 +23,22 @@ class GUI private constructor(component: JSONComponent) {
         shell = Shell(Display.getDefault())
         shell.setSize(1000, 700)
         shell.text = ("JSON GUI")
-        shell.layout = GridLayout(1,false)
+        shell.layout = GridLayout(3,false)
 
-        val group = Group(shell, SWT.NONE);
+        val group = Group(shell, SWT.NONE)
         group.layoutData = GridData(SWT.FILL, SWT.FILL, true, true)
         group.layout = GridLayout(2, true)
 
         tree = Tree(group, SWT.PUSH)
         tree.layoutData = GridData(SWT.FILL, SWT.FILL, true, true)
-
-
         val treeBuilder = JSONTreeBuilder.newTreeBuilder()
         treeBuilder.buildTree(tree, component)
 
         label = Label(group, SWT.FILL)
         label.layoutData = GridData(SWT.FILL, SWT.FILL, true, true)
+
+        textField = Text(group, SWT.NONE)
+        textField.layoutData = GridData(SWT.FILL, SWT.FILL, true, true)
 
         tree.addSelectionListener(object : SelectionAdapter() {
             override fun widgetSelected(e: SelectionEvent) {
@@ -43,6 +46,10 @@ class GUI private constructor(component: JSONComponent) {
                 label.parent.layout()
             }
         })
+
+        textField.addModifyListener {
+            transverse(textField.text, tree.items)
+        }
     }
 
     fun open() {
@@ -58,4 +65,12 @@ class GUI private constructor(component: JSONComponent) {
         display.dispose()
     }
 
+    private fun transverse(keyword: String, items : Array<TreeItem>) {
+        for(item in items) {
+            if(item.text.contains(keyword, true) and keyword.isNotEmpty()) item.background = Color(0,200,255)
+            else item.background = Color(255,255,255)
+
+            if(item.items.isNotEmpty()) transverse(keyword, item.items)
+        }
+    }
 }
